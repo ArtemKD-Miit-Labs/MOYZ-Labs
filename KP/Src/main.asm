e347b379472d6d1d27d08a1ac001aa3ca2cd85f8
libsyfer <libsyfer@gmail.com>
2025-02-25 08:57:31 +0300

    НАписан код программы

--- KP/Src/main.asm
+++ KP/Src/main.asm
@@ -1,18 +1,103 @@
-global _start
+section .data
+    A dw -3, 4, -5       ; массив A
+    B dw 2, -6, 7        ; массив B
+    C dw -8, 9, -10      ; массив C
+    X dw 3               ; коэффициент X
+    Y dw 2               ; коэффициент Y
+    sum1 dd 0            ; сумма (|A[i]| * |B[i]|)
+    sum2 dd 0            ; сумма |C[i]|
+    result dd 0          ; итоговый результат
+    overflow_msg db "Overflow detected!", 0xA, 0
 
-.data
-    A db 100, -43, 33
-    B db 67, 10, -50
-    C db 10, 10, 10
+section .bss
+    temp resw 1
 
-.text
+section .text
+    global _start
+
 _start:
-    mov eax, 10
+    xor eax, eax
+    xor edx, edx
+    mov ecx, 3            ; количество элементов в массивах
+    lea esi, [A]          ; указатель на массив A
+    lea edi, [B]          ; указатель на массив B
+    lea ebx, [C]          ; указатель на массив C
 
-_exit:
+loop_calculation:
+    ; Загружаем |A[i]|
+    mov ax, [esi]
+    cwd                   ; Расширяем AX в DX:AX
+    test ax, ax
+    jns no_abs_A
+    neg ax
+no_abs_A:
+
+    ; Загружаем |B[i]|
+    mov dx, [edi]
+    cwd                   ; Расширяем DX в DX:AX
+    test dx, dx
+    jns no_abs_B
+    neg dx
+no_abs_B:
+
+    ; Умножаем |A[i]| * |B[i]|
+    imul dx
+    jo overflow           ; Проверка переполнения
+
+    ; Складываем сумму sum1 += |A[i]| * |B[i]|
+    add dword [sum1], eax
+    jo overflow           ; Проверка переполнения
+
+    ; Обрабатываем |C[i]|
+    mov ax, [ebx]
+    cwd
+    test ax, ax
+    jns no_abs_C
+    neg ax
+no_abs_C:
+
+    ; Складываем сумму sum2 += |C[i]|
+    add dword [sum2], eax
+    jo overflow           ; Проверка переполнения
+
+    ; Переход к следующему элементу
+    add esi, 2
+    add edi, 2
+    add ebx, 2
+    loop loop_calculation
+
+    ; Умножаем sum1 на X
+    mov eax, [sum1]
+    imul word [X]
+    jo overflow
+    mov [sum1], eax
+
+    ; Умножаем sum2 на Y
+    mov eax, [sum2]
+    imul word [Y]
+    jo overflow
+    mov [sum2], eax
+
+    ; Вычисляем итоговый результат result = sum1 - sum2
+    mov eax, [sum1]
+    sub eax, [sum2]
+    jo overflow
+    mov [result], eax
+
+    ; Завершаем программу
     mov eax, 1
-    mov ebx, 10
-    int 80h
+    mov ebx, [result]
+    int 0x80
 
-; Вычисление sum( |Ai| * |Bi| )
+overflow:
+    ; Обработка переполнения
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, overflow_msg
+    mov edx, 20
+    int 0x80
 
+    ; Завершаем программу с кодом ошибки
+    mov eax, 1
+    mov ebx, 1
+    int 0x80
